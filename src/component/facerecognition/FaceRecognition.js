bf476cb0e41b6b2c2bc3247ab3500edee621c852
MohammedA9 <MohammedA9@Verifone.com>
2021-04-30 18:48:42 +0530

    Integrated with Clarifai API to detect faces

--- src/component/facerecognition/FaceRecognition.js
+++ src/component/facerecognition/FaceRecognition.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl, faceData }) => {
+    const identifyAllFaces = faceData.map((face,i) => 
+    <div key={i} className='bounding-box' style={{top: face.topRow, bottom: face.bottomRow, right: face.rightColumn, left: face.leftColumn}}></div>);
     return (
- <div>hi</div>
+        <div className='ma pic-display'>
+            <div className='absolute mt2'>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
+                {identifyAllFaces}
+            </div>
+        </div>
     );
 }
 
--- src/container/App.js
+++ src/container/App.js
@@ -5,6 +5,78 @@ import ImageLinkForm from '../component/imagelinkform/ImageLinkForm'
 import Rank from '../component/rank/Rank'
 import FaceRecognition from '../component/facerecognition/FaceRecognition'
 import Particles from 'react-particles-js';
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '7a4027fc12d2444097d3ff07dfdd6cae'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      faceData: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Particles className='particles' params={particleParams} />
+        <Navigation />
+        <div className='logorank'>
+          <Logo />
+          <Rank />
+        </div>
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} faceData={this.state.faceData} />
+      </div>
+    );
+  }
+}
+
+  calculateFaceLoctions = (data) => {
+    const faceRegions = data.outputs[0].data.regions;
+    const img = document.getElementById('inputimage');
+    const width = Number(img.width);
+    const height = Number(img.height);
+    const faceData = faceRegions.map((faceRegion) => {
+      let face = faceRegion.region_info.bounding_box;
+      return (
+        {
+          bottomRow: height - face.bottom_row * height,
+          leftColumn: face.left_col * width,
+          rightColumn: width - face.right_col * width,
+          topRow: face.top_row * height
+        })
+    });
+    return faceData;
+  }
+
+  setFaceData = (faceData) => {
+    this.setState({ faceData: faceData });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onSubmit = (event) => {
+    console.log(this.state.input);
+    this.setState({ imageUrl: this.state.input }, () => app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.imageUrl
+      )
+      .then((response) => {
+        this.setFaceData(this.calculateFaceLoctions(response));
+      }).catch((err) => {
+        console.log(err);
+      }));
+  }
 
 const particleParams = {
   "particles": {
@@ -24,20 +96,5 @@ const particleParams = {
     }
   }
 };
-
-function App() {
-  return (
-    <div>
-      <Particles className='particles' params={particleParams} />
-      <Navigation />
-      <div className='logorank'>
-        <Logo />
-        <Rank />
-      </div>
-      <ImageLinkForm />
-      <FaceRecognition />
-    </div>
-  );
-}
 
 export default App;
