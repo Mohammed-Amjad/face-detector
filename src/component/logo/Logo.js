83beed30b3af0c6757e073e1daede48f496b795d
MohammedA9 <MohammedA9@Verifone.com>
2021-05-01 14:44:43 +0530

    Styled navigation and logo

--- src/component/logo/Logo.js
+++ src/component/logo/Logo.js
@@ -6,10 +6,10 @@ import './Logo.css'
 const Logo = () => {
     return (
         <div className="mt0 ma0">
-            <Tilt className="Tilt  logo br2 shadow-2 centre" options={{ max: 50 }} style={{ height: 200, width: 200 }} >
+            <Tilt className="Tilt logo br2 shadow-2 centre" options={{ max: 50 }} style={{ height: 100, width: 100 }} >
                 <div className="Tilt-inner pa3">
-                    <img alt='logo' src={brain} />
-                    <h3>Face Detector</h3>
+                    <img className='mt2' alt='logo' src={brain} height='40' width='40' />
+                    <p className='f7'>Face Detector</p>
                 </div>
             </Tilt>
         </div>
--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import './Navigation.css'
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn === true) {
         return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <nav className="navigation">
                 <p onClick={() => onRouteChange('signin')}
-                    className='f3 link dim black underline pa3 pointer'>Sign out</p>
+                    className='link dim black underline pa3 pointer'>Sign out</p>
             </nav>);
     } else {
         return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <nav className='navigation'>
                 <p onClick={() => onRouteChange('signin')}
-                    className='f3 link dim black underline pa3 pointer'>Sign in</p>
+                    className='link dim black underline pa3 pointer'>Sign in</p>
                 <p onClick={() => onRouteChange('register')}
-                    className='f3 link dim black underline pa3 pointer'>Register</p>
+                    className='link dim black underline pa3 pointer'>Register</p>
             </nav>
         );
     }
